1aed4b78d52917373dcf737a7377d9d2f3012397
Syahrul Safarila <sysafarila.official@gmail.com>
2025-10-08 22:08:00 +0700

    refactor(OverlayManager): dynamic options

--- src/base/OverlayManager.ts
+++ src/base/OverlayManager.ts
@@ -2,17 +2,49 @@ import Overlay from "./Overlay";
 import Queue from "./Queue";
 import Listener from "./Listener";
 import Adapter from "./Adapter";
+import SaweriaAdapter from "../adapters/saweria/SaweriaAdapter";
+import Formula1Overlay from "../overlays/Formula1Overlay";
+
+type Provider = "Saweria" | "BagiBagi"
+type OverlayType = "Basic" | "Formula 1"
+type Options = {
+    provider: Provider;
+    overlayType: OverlayType;
+    sourceOverlayUrl: string;
+}
 
 export default class OverlayManager {
-    init() {
+    init(options: Options) {
         console.info("Connecting...")
         const htmlElement = document.querySelector('#overlay')
         if (!htmlElement) throw new Error("No overlay element found");
 
-        const sourceOverlayUrl: string = "https://saweria.co/widgets/alert?streamKey=a4cb06bb44033504e163d68c0d66f1e3";
+        const sourceOverlayUrl: string = options.sourceOverlayUrl;
 
-        const adapter = new Adapter(sourceOverlayUrl);
-        const overlay = new Overlay(htmlElement)
+        let adapter: Adapter;
+        switch (options.provider) {
+            case "Saweria":
+                adapter = new SaweriaAdapter(sourceOverlayUrl)
+                break;
+            case "BagiBagi":
+                adapter = new Adapter(sourceOverlayUrl)
+                break;
+            default:
+                adapter = new Adapter(sourceOverlayUrl);
+        }
+
+        let overlay: Overlay;
+        switch (options.overlayType) {
+            case "Basic":
+                overlay = new Overlay(htmlElement);
+                break;
+            case "Formula 1":
+                overlay = new Formula1Overlay(htmlElement);
+                break;
+            default:
+                overlay = new Overlay(htmlElement);
+        }
+
         const queue = new Queue(overlay)
         const listener = new Listener(queue, adapter)
         listener.listen()
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import OverlayManager from "./base/OverlayManager";
 
 const overlayManager = new OverlayManager();
-overlayManager.init();
+overlayManager.init({
+    overlayType: 'Basic',
+    provider: 'Saweria',
+    sourceOverlayUrl: 'https://saweria.co/widgets/alert?streamKey=a4cb06bb44033504e163d68c0d66f1e3'
+});
