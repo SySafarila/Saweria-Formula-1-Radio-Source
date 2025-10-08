2fbdd6bd9ef2c316ca651f16cf51f1bf98bcf4e3
Syahrul Safarila <sysafarila.official@gmail.com>
2025-10-09 00:36:18 +0700

    refactor: bagibagi.co

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import OverlayManager from "./base/OverlayManager";
 
 const overlayManager = new OverlayManager();
 overlayManager.init({
-    overlayType: 'Basic',
-    provider: 'Saweria',
+    overlayType: 'Formula 1',
+    provider: 'BagiBagi',
     parentHtmlElement: document.querySelector('#overlay'),
-    sourceOverlayUrl: 'https://saweria.co/widgets/alert?streamKey=a4cb06bb44033504e163d68c0d66f1e3'
+    sourceOverlayUrl: 'https://bagibagi.co/alertbox/0ZzTRCQsIE1qQ4Z3HANFdTZqTZbKoMga'
 });
